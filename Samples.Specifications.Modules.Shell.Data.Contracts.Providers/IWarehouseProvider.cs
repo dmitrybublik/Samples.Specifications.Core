864342ea4009ada97829a8adefb715a3d2d70236
Dmitry Bublik <dmitry.buba@gmail.com>
2020-04-28 16:14:49 +0400

    feat: add data projects

---
+++ Samples.Specifications.Modules.Shell.Data.Contracts.Providers/IWarehouseProvider.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using Samples.Specifications.Modules.Shell.Data.Contracts.Dto;
+
+namespace Samples.Specifications.Modules.Shell.Data.Contracts.Providers
+{
+    public interface IWarehouseProvider
+    {
+        IEnumerable<WarehouseItemDto> GetWarehouseItems();
+        bool DeleteWarehouseItem(Guid id);
+        bool UpdateWarehouseItem(WarehouseItemDto dto);
+        void CreateWarehouseItem(WarehouseItemDto dto);
+    }
+}
---
+++ Samples.Specifications.Modules.Shell.Data.Fake.Providers/Module.cs
@@ -0,0 +1,16 @@
+﻿using JetBrains.Annotations;
+using Samples.Specifications.Modules.Shell.Data.Contracts.Providers;
+using Solid.Practices.IoC;
+using Solid.Practices.Modularity;
+
+namespace Samples.Specifications.Modules.Shell.Data.Fake.Providers
+{
+    [UsedImplicitly]
+    internal sealed class Module : ICompositionModule<IDependencyRegistrator>
+    {
+        public void RegisterModule(IDependencyRegistrator dependencyRegistrator)
+        {
+            dependencyRegistrator.AddSingleton<IWarehouseProvider, FakeWarehouseProvider>();
+        }
+    }
+}
